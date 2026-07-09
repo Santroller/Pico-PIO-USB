d035acca38c6f02391423da47311b264e78c3ce5
Sanjay Govind <sanjay.govind9@gmail.com>
2026-07-09 14:38:40 +1200

    remove __time_critical_func from update_usb_crc16
    __time_critical_func implies noinline, but inlining the function is much faster

--- src/usb_crc.h
+++ src/usb_crc.h
@@ -9,7 +9,7 @@ uint8_t calc_usb_crc5(uint16_t data);
 uint16_t calc_usb_crc16(const uint8_t *data, uint16_t len);
 
 extern const uint16_t crc16_tbl[256];
-static inline uint16_t __time_critical_func(update_usb_crc16)(uint16_t crc, uint8_t data) {
+static inline uint16_t update_usb_crc16(uint16_t crc, uint8_t data) {
   crc = (crc >> 8) ^ crc16_tbl[(crc ^ data) & 0xff];
   return crc;
 }
